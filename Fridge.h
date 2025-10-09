98c37440f6b6719045b8e54ca843c2069032a485
zy-08-jigongben-02 <745519899@qq.com>
2025-10-09 15:58:51 +0800

    Create Fridge.h

---
+++ Fridge.h
@@ -0,0 +1,14 @@
+#ifndef FRIDGE_H
+#define FRIDGE_H
+
+class Fridge {
+private:
+    bool doorOpen;
+public:
+    Fridge();
+    void openDoor();
+    void closeDoor();
+    void putElephant();
+};
+
+#endif
