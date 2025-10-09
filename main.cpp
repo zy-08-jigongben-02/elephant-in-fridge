5817728e40afe415bf1683e1bb3990be493199c7
zy-08-jigongben-02 <745519899@qq.com>
2025-10-09 16:02:13 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Elephant.h"
+using namespace std;
+
+int main() {
+    Fridge myFridge;
+    Elephant myElephant("胖胖");
+    myElephant.enterFridge(myFridge);
+
+    system("pause");
+    return 0;
+}
